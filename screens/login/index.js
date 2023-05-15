9a10bf4600f79de9a92d13eb3e808558b5834615
Edward Sarfo <edsarfo1@gmail.com>
2023-05-15 14:16:56 +0100

    async

--- screens/login/index.js
+++ screens/login/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
+import {View, Text, TextInput, Button, StyleSheet, Alert} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Dashboard from '../dashboard';
 
-const Login = ({navigation}) => {
+const LoginScreen = ({navigation}) => {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
 
-  const handleLogin = () => {
-    navigation.navigate('Dashboard');
+  const handleLogin = async () => {
+    try {
+      // get list ofusers from AsyncStorage
+      const users = await AsyncStorage.getItem('users');
+      console.log(users);
+      const parsedUsers = JSON.parse(users);
+      console.log(parsedUsers);
+
+      // Find the user with this username and password
+      const user = parsedUsers.find(
+        user => user.username === username && user.password === password,
+      );
+      console.log(user);
+
+      if (user) {
+        // If it exists, navigate to Dashboard
+        navigation.navigate('Dashboard');
+      } else {
+        // If not, show an error message
+        Alert.alert('Error', 'Invalid username or password');
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Error', 'Failed to login');
+    }
   };
 
   return (
@@ -57,4 +81,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Login;
+export default LoginScreen;
